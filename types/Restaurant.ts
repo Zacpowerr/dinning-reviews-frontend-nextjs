dc8d8eea985dbfe502230df28afbfa8c6dcf29ec
Eduardo Bastos <e_kempfer@hotmail.com>
2025-06-18 12:01:21 -0600

    Add Restaurant and Review type definitions

---
+++ types/Restaurant.ts
@@ -0,0 +1,9 @@
+export type Restaurant = {
+  id: number;
+  name: string;
+  overallScore: number;
+  peanutScore: number;
+  eggScore: number;
+  dairyScore: number;
+  zipCode: string;
+};
---
+++ types/Review.ts
@@ -0,0 +1,10 @@
+export type Review = {
+  id: number;
+  userName: string;
+  restaurantId: number;
+  peanutScore: number;
+  eggScore: number;
+  dairyScore: number;
+  details: string;
+  status: string;
+};
