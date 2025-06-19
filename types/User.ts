29429e3079d12cd05448f829f3e657227aa65f55
Eduardo Bastos <e_kempfer@hotmail.com>
2025-06-19 09:54:54 -0600

    Add User type definition for user-related data structure

---
+++ types/User.ts
@@ -0,0 +1,10 @@
+export type User = {
+  id: number;
+  userName: string;
+  city: string;
+  state: string;
+  zipcode: string;
+  hasPeanutAllergy: boolean;
+  hasEggAllergy: boolean;
+  hasDairyAllergy: boolean;
+};
